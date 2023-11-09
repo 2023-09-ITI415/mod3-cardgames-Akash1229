d9bb3a42d44e1165ac537a0137aa7089bac288e0
Akash1229 <adp180@rutgers.edu>
2023-11-09 12:08:51 -0500

    Pyramidwork

--- Assets/01-Prospector/__Scripts/Layout.cs
+++ Assets/01-Prospector/__Scripts/Layout.cs
@@ -82,5 +82,40 @@ public class Layout : MonoBehaviour
             }
         }
     }
+    public void LayOutPyramid()
+    {
+        float yOffset = 0.0f; // Adjust this value to control the vertical spacing between rows.
+
+        for (int row = 0; row < numRows; row++)
+        {
+            int numSlotsInRow = row + 1; // Number of slots in the current row
+            float rowWidth = numSlotsInRow * cardWidth;
+
+            for (int slotIndex = 0; slotIndex < numSlotsInRow; slotIndex++)
+            {
+                SlotDef slot = slotDefs[slotIndex]; // Retrieve the appropriate slot definition
+                Vector3 slotPosition = new Vector3(slot.x + (slotIndex * cardWidth), yOffset, 0f);
+
+                // Set the position of the card or game object based on the slot definition
+                // Here, you should adjust the position according to the slotPosition.
+                // For example, you can move the card's transform to the slotPosition.
+
+                yOffset += cardHeight; // Move to the next row
+            }
+        }
+    }
+
+    // Call this method to lay out cards in a pyramid.
+    public void LayOutPyramidCards()
+    {
+        // Assuming you have defined cardWidth and cardHeight.
+        float cardWidth = 1.0f; // Change this to your card's width
+        float cardHeight = 1.5f; // Change this to your card's height
+
+        // Assuming numRows is the number of rows in the pyramid.
+        int numRows = 5; // Adjust this value based on your pyramid layout.
+
+        LayOutPyramid();
+    }
 
 }
